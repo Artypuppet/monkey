f97ce2a69227494ea3585807c7378351b335335e
Artypuppet <jaspreetsingh8437055402@gmail.com>
2024-05-06 13:33:06 -0400

    lexer is able to recognize int, identifiers and functions. NOT IMPLEMENTED:  conditionals, booleans etc.

---
+++ lexer/lexer.go
@@ -0,0 +1,122 @@
+package lexer
+
+import (
+	token "github.com/Artypuppet/monkey/token"
+)
+
+// TODO change the lexer to support UTF-8 encoding.
+// this struct defines the lexer for our language which
+// creates tokens character by character
+type Lexer struct {
+	input        string // The input file/string
+	ch           byte   // the current character in input
+	position     int    // represents the index of the current ch character in the input
+	readPosition int    // represents the index of the next character after ch in the input
+}
+
+// constructor for lexer
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// helper method to get the next character in the input string
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() *token.Token {
+	var tok *token.Token
+	// ignore any whitespace between characters
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok = newToken(token.EOF, 0)
+		tok.Literal = ""
+	default:
+		if isLetter(l.ch) {
+			literal := l.readIdentifier()
+			return &token.Token{Type: token.LookupIdent(literal), Literal: literal}
+		} else if isDigitFirst(l.ch) {
+			return &token.Token{Type: token.INT, Literal: l.readDigit()}
+		} else {
+			return newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+// TODO handle cases where when reading the identifier we encounter a char that is not a letter e.g. %
+// helper method to read the an identifier
+func (l *Lexer) readIdentifier() string {
+	initialPos := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[initialPos:l.position]
+}
+
+// TODO handle cases where we have a float or right after a digit we have a non digit char
+// for floats if we have a number like 9. we interpret it it as a float rather than throwing an error.
+// helper function to determine whether
+func (l *Lexer) readDigit() string {
+	initialPos := l.position
+	for isDigitFirst(l.ch) {
+		l.readChar()
+	}
+	return l.input[initialPos:l.position]
+}
+
+// helper method to ignore whitespace between characters
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// function to create a new token with the given TokenType and Literal value ch and returns a pointer to it.
+func newToken(tokenType token.TokenType, ch byte) *token.Token {
+	return &token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// function to determine whether the character is a part of the language
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// function that checks if the character is a digit or a plus or a minus sign
+// only used for the first character when identifying a number
+func isDigitFirst(ch byte) bool {
+	return isDigit(ch) || ch == '-' || ch == '+'
+}
+
+// function that check if a character is a digit.
+func isDigit(ch byte) bool {
+	return ('0' <= ch && ch <= '9')
+}
---
+++ token/token.go
@@ -0,0 +1,74 @@
+package token
+
+// type def for string to TokenType to signify the type of
+// the token e.g. INT or FUNCTION etc. This helps distinguish
+// whether token is a literal, identifier or keyword.
+type TokenType string
+
+// struct defining the Tokens in the language
+// each token has a TokenType and the value/literal
+// of it e.g. in the experession let x = 5
+// 'let' is a keyword with Literal value of 'let'
+// while 'x' is an IDENTIFIER with a Literal value of 'x' and so on.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Following are the possible TokenTypes in the language
+// ILLEGAL specifies an unknown token type while EOF stand for "end of file"
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// map to emulate a set for faster lookup than switch
+var TokenTypes = map[TokenType]struct{}{
+	"ILLEGAL": {},
+	"":        {}, // Eof
+	// Identifiers + literals
+	"IDENT": {}, // add, foobar, x, y, ...
+	"INT":   {}, // 1343456
+	// Operators
+	"=": {},
+	"+": {},
+	// Delimiters
+	",": {},
+	";": {},
+	"(": {},
+	")": {},
+	"{": {},
+	"}": {},
+	// Keywords
+	"FUNCTION": {},
+	"LET":      {},
+}
+
+var Idents = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// function that determines whether a string literal is a keyword or an Identifier
+func LookupIdent(literal string) TokenType {
+	if tokenType, ok := Idents[literal]; ok {
+		return tokenType
+	}
+	return IDENT
+}
